391894ba8a65f49020ed89fd9466cbc773c6d5a6
Denis <92040760+denkosz@users.noreply.github.com>
2023-03-01 14:07:39 +0100

    Add files via upload

---
+++ web.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import functions1
+# streamlit run C:\PythoProjects\pythonProject3\webapp\web.py
+# pip freeze > requirements.txt
+todos = functions1.get_todos()
+
+
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions1.write_todos(todos)
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions1.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="Enter todo:", placeholder="Enter here",
+              on_change=add_todo, key="new_todo")
