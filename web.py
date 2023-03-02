720e8ff16e468b9275dd0098c872022e5f50ccd7
Denis <92040760+denkosz@users.noreply.github.com>
2023-03-02 13:26:56 +0100

    Update web.py

--- web.py
+++ web.py
@@ -15,7 +15,7 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("<h1>This app is to increase your <b>productivity.<b></h1>",
+st.write("<h3>This app is to increase your <b>productivity.<b></h3>",
          unsafe_allow_html=True)
 
 st.text_input(label="Enter todo:", placeholder="Enter here",
